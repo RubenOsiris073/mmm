aab72a505808c4c42e671592e96afeebbd928033
RubenOsiris073 <rube.nr6c@gmail.com>
2025-07-15 13:25:19 -0600

    stripe0.3

--- backend/config/cors.js
+++ backend/config/cors.js
@@ -4,6 +4,7 @@ const corsOptions = {
     'http://127.0.0.1:3000',
     'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'https://psychic-bassoon-j65x4rxrvj4c5p54-3000.app.github.dev',
     process.env.FRONTEND_URL
   ].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
